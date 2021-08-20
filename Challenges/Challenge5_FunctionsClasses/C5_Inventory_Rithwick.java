1597778227cee79a27a219127e3697aabfe7d813
RithwickL <88684221+RithwickL@users.noreply.github.com>
2021-08-20 09:28:59 -0500

    started challegne 5

---
+++ Challenges/Challenge5_FunctionsClasses/C5_Inventory_Rithwick.java
@@ -0,0 +1,18 @@
+package Challenges.Challenge5_FunctionsClasses;
+
+/**
+ * Write a description of class C5_Inventory_Rithwick here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class C5_Inventory_Rithwick
+{
+    public static void main(String args[])
+    {
+    //Subaru
+
+       C5_Subaru2;
+       Subaru = new Subaru3("Subaru", 2020, 100, 50000.00);
+    }
+}
---
+++ Challenges/Challenge5_FunctionsClasses/Car_Rithwick.java
@@ -0,0 +1,20 @@
+package Challenges.Challenge5_FunctionsClasses;
+
+
+/**
+ * Write a description of class Car_Rithwick here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Car_Rithwick
+{   
+    public C5_Subaru2 (String brand, int year, int inv, double price )
+    {
+        carmake  = brand;
+        releaseyear = year;
+        carsinv = inv;
+        avgprice = price;
+    
+    }
+}
